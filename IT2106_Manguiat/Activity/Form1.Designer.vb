386d91cddd97429bbb6a0fdb63292a2957af1ea3
LintiknaPag <70244307+LintiknaPag@users.noreply.github.com>
2022-12-21 18:53:57 +0700

    Add files via upload

---
+++ IT2106_Manguiat/Activity/Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtNetpay = New System.Windows.Forms.TextBox()
+        Me.txtFica = New System.Windows.Forms.TextBox()
+        Me.txtFtw = New System.Windows.Forms.TextBox()
+        Me.txtGross = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(776, 47)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(469, 44)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 84)
+        Me.ListBox1.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.ListBox2)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(10, 86)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(933, 161)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(617, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 16)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Rate:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(466, 25)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 16)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Hours:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(773, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 16)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Allowances:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 16)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Name:"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(292, 84)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(74, 20)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Married"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(292, 47)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(66, 20)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Single"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(6, 63)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(222, 22)
+        Me.txtName.TabIndex = 3
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(620, 44)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 84)
+        Me.ListBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Impact", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(314, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(344, 37)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Manguiat Company Payroll"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtNetpay)
+        Me.GroupBox2.Controls.Add(Me.txtFica)
+        Me.GroupBox2.Controls.Add(Me.txtFtw)
+        Me.GroupBox2.Controls.Add(Me.txtGross)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Location = New System.Drawing.Point(10, 289)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(619, 99)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        '
+        'txtNetpay
+        '
+        Me.txtNetpay.Location = New System.Drawing.Point(484, 43)
+        Me.txtNetpay.Name = "txtNetpay"
+        Me.txtNetpay.Size = New System.Drawing.Size(96, 22)
+        Me.txtNetpay.TabIndex = 10
+        '
+        'txtFica
+        '
+        Me.txtFica.Location = New System.Drawing.Point(348, 43)
+        Me.txtFica.Name = "txtFica"
+        Me.txtFica.Size = New System.Drawing.Size(93, 22)
+        Me.txtFica.TabIndex = 11
+        '
+        'txtFtw
+        '
+        Me.txtFtw.Location = New System.Drawing.Point(197, 43)
+        Me.txtFtw.Name = "txtFtw"
+        Me.txtFtw.Size = New System.Drawing.Size(88, 22)
+        Me.txtFtw.TabIndex = 12
+        '
+        'txtGross
+        '
+        Me.txtGross.Location = New System.Drawing.Point(48, 43)
+        Me.txtGross.Name = "txtGross"
+        Me.txtGross.Size = New System.Drawing.Size(88, 22)
+        Me.txtGross.TabIndex = 13
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(375, 18)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(39, 16)
+        Me.Label13.TabIndex = 7
+        Me.Label13.Text = "FICA:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(506, 18)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(57, 16)
+        Me.Label9.TabIndex = 3
+        Me.Label9.Text = "Net pay:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(219, 18)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 16)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "FWT:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(54, 18)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(72, 16)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Gross pay:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(694, 322)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 42)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(787, 322)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 42)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(966, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtNetpay As TextBox
+    Friend WithEvents txtFica As TextBox
+    Friend WithEvents txtFtw As TextBox
+    Friend WithEvents txtGross As TextBox
+End Class
---
+++ IT2106_Manguiat/Activity/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+    Dim intAllow As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        For intAllow As Integer = 0 To 10
+            ComboBox1.Items.Add(intAllow)
+        Next intAllow
+
+        For dblHours = 1 To 1000 Step 0.5
+            ListBox1.Items.Add(dblHours)
+        Next dblHours
+
+        For dblRates = 1 To 1000 Step 0.5
+            ListBox2.Items.Add(dblRates)
+        Next dblRates
+    End Sub
+
+    Private Sub ComboBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ComboBox1.KeyPress
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") _
+            AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged,
+           ListBox1.SelectedIndexChanged, ListBox2.SelectedIndexChanged, RadioButton1.CheckedChanged, RadioButton2.CheckedChanged, txtName.TextChanged
+
+        txtGross.Text = String.Empty
+        txtFtw.Text = String.Empty
+        txtFica.Text = String.Empty
+        txtNetpay.Text = String.Empty
+
+    End Sub
+
+    Private Sub GetSingleFwt(ByVal dblTaxPay As Double, ByRef dblFedWthTax As Double)
+        Select Case dblTaxPay
+            Case <= 44
+                dblFedWthTax = 0
+            Case <= 224
+                dblFedWthTax = 0.1 * (dblTaxPay - 44)
+            Case <= 774
+                dblFedWthTax = 18 + 0.15 * (dblTaxPay - 224)
+            Case <= 1812
+                dblFedWthTax = 100.5 + 0.25 * (dblTaxPay - 774)
+            Case <= 3730
+                dblFedWthTax = 360 + 0.28 * (dblTaxPay - 1812)
+            Case <= 8058
+                dblFedWthTax = 897.04 + 0.33 * (dblTaxPay - 3730)
+            Case <= 8090
+                dblFedWthTax = 2325.28 + 0.35 * (dblTaxPay - 3730)
+            Case Else
+                dblFedWthTax = 2336.48 + 0.396 * (dblTaxPay - 8090)
+        End Select
+
+    End Sub
+
+
+    Private Function GetMarriedFwt(ByVal dblTaxPay As Double) As Double
+        Dim dblFedWthTax As Double
+
+        Select Case dblTaxPay
+            Case <= 166
+                dblFedWthTax = 0
+            Case <= 525
+                dblFedWthTax = 0.1 * (dblTaxPay - 166)
+            Case <= 1626
+                dblFedWthTax = 35.9 + 0.15 * (dblTaxPay - 525)
+            Case <= 3111
+                dblFedWthTax = 201.5 + 0.25 * (dblTaxPay - 1626)
+            Case <= 4654
+                dblFedWthTax = 572.3 + 0.28 * (dblTaxPay - 3111)
+            Case <= 8180
+                dblFedWthTax = 1004.34 + 0.33 * (dblTaxPay - 4654)
+            Case <= 9128
+                dblFedWthTax = 2167.92 + 0.35 * (dblTaxPay - 8180)
+            Case Else
+                dblFedWthTax = 2531.22 + 0.396 * (dblTaxPay - 9128)
+        End Select
+
+        Return dblFedWthTax
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Const dblONE_ALLOWANCE As Double = 77.9
+        Const dblFICA_RATE As Double = 0.0765
+        Dim dblHours As Double
+        Dim dblPayRate As Double
+        Dim intAllowances As Integer
+        Dim dblGross As Double
+        Dim dblTaxable As Double
+        Dim dblFwt As Double
+        Dim dblFica As Double
+        Dim dblNet As Double
+
+        Double.TryParse(ListBox1.SelectedItem.ToString, dblHours)
+        Double.TryParse(ListBox2.SelectedItem.ToString, dblPayRate)
+        Integer.TryParse(ComboBox1.Text, intAllowances)
+
+        If dblHours <= 40 Then
+            dblGross = dblHours * dblPayRate
+        Else
+            dblGross = 40 * dblPayRate + (dblHours - 40) * dblPayRate * 1.5
+        End If
+
+        dblTaxable = dblGross - (intAllowances * dblONE_ALLOWANCE)
+
+        If RadioButton1.Checked = True Then
+            GetSingleFwt(dblTaxable, dblFwt)
+
+        Else
+            dblFwt = GetMarriedFwt(dblTaxable)
+        End If
+
+
+        dblFica = dblGross * dblFICA_RATE
+
+        dblGross = Math.Round(dblGross, 2)
+        dblFwt = Math.Round(dblFwt, 2)
+        dblFica = Math.Round(dblFica, 2)
+
+        dblNet = dblGross - dblFwt - dblFica
+
+        txtGross.Text = dblGross.ToString("N2")
+        txtFtw.Text = dblFwt.ToString("N2")
+        txtFica.Text = dblFica.ToString("N2")
+        txtNetpay.Text = dblNet.ToString("N2")
+
+
+
+    End Sub
+
+End Class
+
+
+
